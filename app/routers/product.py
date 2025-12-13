36a4905d110963847cd66ae838da518ce7eb07d6
djumanov <djumanovdev@gmail.com>
2025-12-13 09:50:01 +0500

    fix: check category

--- app/routers/product.py
+++ app/routers/product.py
@@ -1,12 +1,12 @@
 from typing import Annotated
 
 from fastapi.routing import APIRouter
-from fastapi import Body, Path, Query, Depends
+from fastapi import Body, Path, Query, Depends, HTTPException, status
 from sqlalchemy.orm import Session
 
 from ..schemas import ProductCreate, ProductResponse
 from ..database import get_db
-from ..models import Product
+from ..models import Product, Category
 
 router = APIRouter(tags=['Products'])
 
@@ -22,10 +22,14 @@ def create_product(
     product_data: Annotated[ProductCreate, Body],
     session: Annotated[Session, Depends(get_db)]
 ):
+    category = session.query(Category).get(product_data.category_id)
+    if category is None:
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail='Category not found')
+    
     product = Product(
         name=product_data.name,
         price=product_data.price,
-        category_id=product_data.category_id,
+        category_id=category.category_id,
         in_stock=product_data.in_stock
     )
     session.add(product)
